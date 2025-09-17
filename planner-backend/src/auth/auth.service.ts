11b7e1d6c669456e3557fd16810106c78a8d9daa
codeShark <tokesh04@mail.ru>
2025-09-17 22:46:28 +0500

    feat(auth): add login logic

---
+++ planner-backend/src/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import {
+	Injectable,
+	NotFoundException,
+	UnauthorizedException
+} from '@nestjs/common'
+import { UserService } from 'src/user/user.service'
+import { AuthDto } from './dto/auth.dto'
+import { JwtService } from '@nestjs/jwt'
+import { verify } from 'argon2'
+
+@Injectable()
+export class AuthService {
+	constructor(
+		private jwt: JwtService,
+		private userService: UserService
+	) {}
+
+	async login(dto: AuthDto) {
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { password, ...user } = await this.validateUser(dto)
+		const tokens = this.issueTokens(user.id)
+
+		return { user, tokens }
+	}
+
+	private issueTokens(userId: string) {
+		const data = { id: userId }
+
+		const accessToken = this.jwt.sign(data, {
+			expiresIn: '1h'
+		})
+
+		const refreshToken = this.jwt.sign(data, {
+			expiresIn: '7d'
+		})
+
+		return { accessToken, refreshToken }
+	}
+
+	private async validateUser(dto: AuthDto) {
+		const user = await this.userService.getByEmail(dto.email)
+
+		if (!user) throw new NotFoundException('User not found')
+
+		const isValid = await verify(user.password, dto.password)
+
+		if (!isValid) throw new UnauthorizedException('Invalid password')
+
+		return user
+	}
+}
